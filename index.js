6d603768952bdf3db068122019afc4f4c2d7ccea
mohamad-ridwan <mr643062@gmail.com>
2021-11-03 08:46:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const multer = require('multer')
+const path = require('path')
+const cors = require('cors')
+const dotenv = require('dotenv')
